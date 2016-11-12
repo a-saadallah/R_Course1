763ca58f55aa658c8c9a4efde1038b70e97e6196
a-saadallah <ahmed.ebaid@gmx.com>
2016-11-12 21:55:12 +0100

    first part

---
+++ Indexing.R
@@ -0,0 +1,19 @@
+# Index
+
+# Generate 2x3 marix
+X <− matrix(c (4 , 7 , 3 , 8 , 9 , 2) , nrow = 2)
+X
+
+X[2,2]
+X[,2]
+?rnorm
+numbers_1 <− rnorm (80 ,mean=0,sd=1)
+mat_1 <− matrix( numbers_1 ,nrow=20, ncol=4)
+mat_1
+
+# convert into data frame (with headers)
+df_1 <− data.frame (mat_1)
+df_1
+names(df_1)<-c("var1","var2","var3","var4")
+df_1
+head(df_1)
