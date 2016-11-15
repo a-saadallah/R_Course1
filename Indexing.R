5bcd323b50fd2a4106e9e620b79c30b68b4a9d34
a-saadallah <ahmed.ebaid@gmx.com>
2016-11-15 11:58:41 +0100

    indexing

--- Indexing.R
+++ Indexing.R
@@ -17,3 +17,9 @@ df_1
 names(df_1)<-c("var1","var2","var3","var4")
 df_1
 head(df_1)
+
+df<−data.frame(plot="location_name_1", measure1=runif(100)∗1000,measure2=round(runif(100)∗100),value=rnorm (100 ,2 ,1) ,ID=rep (LETTERS,100))
+
+df[df$value>3.0,]
+
+df [ df$value >3.2 | df$measure1>950 ,]
